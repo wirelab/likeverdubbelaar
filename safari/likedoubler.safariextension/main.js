eb01f1605b3c040912128f62f09cf7c353b96b0d
Johan Bruning <johan@taquito.nl>
2014-03-28 10:03:54 +0100

    Make sure we have  page to tell people it was a joke

--- safari/likedoubler.safariextension/main.js
+++ safari/likedoubler.safariextension/main.js
@@ -4,12 +4,6 @@ new_likes = likes * 2
 
 var trailing = isNaN(original_value.slice(-1))
 
-if(trailing) {
-    var trailing_char = original_value.slice(-1)
-    $('#pagelet_timeline_likes_nav_top span.stats').html("" +new_likes +trailing_char)
-} else {
-  $('#pagelet_timeline_likes_nav_top span.stats').html(new_likes)
-}
 var full_content = $('#fbTimelineHeadline .fsm.fwn.fcg').html()
 var original_full_value = full_content.match(/[\d(\,|\.)]+(?=\s[likes|personen vinden dit leuk])/g)[0]
 
@@ -25,4 +19,18 @@ if(original_full_value.indexOf(',') === -1) {
 new_like_count = accounting.formatNumber((number * 2), 0, seperator)
 
 var replace_content = full_content.replace(original_full_value.toString(), new_like_count)
-$('#fbTimelineHeadline .fsm.fwn.fcg').html(replace_content)
+
+var date = new Date()
+if((date.getMonth() === 3 && date.getDay() > 1) || (date.getMonth() > 3)) {
+  var expired_content = full_content + "<br /><p><a href='http://www.wirelab.nl/aprilfools?utm_source=likedoubler&utm_medium=extension'>Your free like doubler trial has expired</a><p>"
+  $('#fbTimelineHeadline .fsm.fwn.fcg').html(expired_content);
+} else {
+  if(trailing) {
+      var trailing_char = original_value.slice(-1)
+      $('#pagelet_timeline_likes_nav_top span.stats').html("" +new_likes +trailing_char)
+  } else {
+    $('#pagelet_timeline_likes_nav_top span.stats').html(new_likes)
+  }
+
+  $('#fbTimelineHeadline .fsm.fwn.fcg').html(replace_content)
+}
